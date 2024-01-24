75009d1638e3493950c9c96396c22027e5fdfad4
admin <admin@DESKTOP-CRB7MLP>
2024-01-24 17:50:36 +0900

    d

--- hgjava/src/com/yedam/HelloWorld.java
+++ hgjava/src/com/yedam/HelloWorld.java
@@ -3,5 +3,10 @@ package com.yedam;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World");
+		String hi = "    1234    ";
+		String th = hi.trim();
+		char a = hi.charAt(2);
+		System.out.println(hi);
+		System.out.println(th);
 	}
 }
